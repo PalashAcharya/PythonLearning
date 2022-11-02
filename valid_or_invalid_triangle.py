a2908335c3183dc8997b1b815aabae39bf8dca9f
PalashAcharya <palashacharya2004@gmail.com>
2022-11-02 21:54:22 +0530

    added new programs

---
+++ valid_or_invalid_triangle.py
@@ -0,0 +1,7 @@
+x = input("Enter the length of the first side of the triangle:")
+
+y = input("Enter the length of the second side of the triangle:")
+
+z= input("Enter the length of the third side of the triangle:")
+
+if(x+y>z                            
