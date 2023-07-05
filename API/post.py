fb2d43f6b125188341241494d2d4cd452ae4eefd
PalashAcharya <palashacharya2004@gmail.com>
2023-07-05 20:17:57 +0530

    added some changes

--- API/post.py
+++ API/post.py
@@ -1,14 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 class Students(BaseModel):
-    ID:int
-    Name:str
-    Phone:Optional[str] = None
-    Area:str
+    ID:int 
+    Name:str = Field(examples = "palash")
+    Phone:str | None = Field(default=None,title="The phone number of student",max_length=10,description="The phone number should be 10 digits only",examples="8200087138" )
+    Area:str = Field(examples="Bodakdev")
 data = {}
 
 @app.post("/Add/{id}")
