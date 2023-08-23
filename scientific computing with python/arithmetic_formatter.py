b9e745f63410bc790b5e4db250b60ca73eb21a1f
PalashAcharya <palashacharya2004@gmail.com>
2023-08-23 18:31:42 +0530

    handled errors

--- scientific computing with python/arithmetic_formatter.py
+++ scientific computing with python/arithmetic_formatter.py
@@ -1,7 +1,7 @@
 def artithmetic_arranger(List,bool):
     length_ = len(List)
     if length_ >5:
-        print("List must not have more than 5 operations")
+        raise ValueError("There must only be 5 operations")
     tuple_list = []
     result_list=[]
     for elements in List:
@@ -9,6 +9,10 @@ def artithmetic_arranger(List,bool):
             tuple_list.append(list(elements.partition("+")))
         elif "-" in elements:
             tuple_list.append(list(elements.partition("-")))
+        elif "*" in elements:
+            raise ValueError("Operator must be '+' or '-'")
+        elif "/" in elements:
+            raise ValueError("Operator must be '+' or '-'")
     for i in range(length_):
         for j in range(3):
             if j==1:
@@ -19,15 +23,21 @@ def artithmetic_arranger(List,bool):
     if(bool==True):
         for var in tuple_list:
             if(var[1]=="+"):
-                op1 = int(var[0])
-                op2 = int(var[2])
-                res = op1+op2
-                result_list.append(str(res))
+                try:
+                    op1 = int(var[0])
+                    op2 = int(var[2])
+                    res = op1+op2
+                    result_list.append(str(res))
+                except:
+                    raise ValueError("Operands can only be digits")
             else:
-                op1 = int(var[0])
-                op2 = int(var[2])
-                res=op1-op2
-                result_list.append(str(res))
+                try:
+                    op1 = int(var[0])
+                    op2 = int(var[2])
+                    res=op1-op2
+                    result_list.append(str(res))
+                except:
+                    raise ValueError("Operands can only be digits")
         adj_result_str=result_list[0].rjust(5)
         result_String = adj_result_str
         for var in range(1,length_):
@@ -54,4 +64,4 @@ def Format(element_list,length):
         return -1
 
 L1 = ["1234+4231","1234+2421","1234+3321","1123+1234","9999+9999"]
-print(artithmetic_arranger(L1,False))
+print(artithmetic_arranger(L1,True))
