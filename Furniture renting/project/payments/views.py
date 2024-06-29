a7c654ef82a5ee71b2de36ea30e1569519dc6a40
PalashAcharya <palashacharya2004@gmail.com>
2024-06-29 17:02:00 +0530

    added changes

--- Furniture renting/project/payments/views.py
+++ Furniture renting/project/payments/views.py
@@ -11,30 +11,40 @@ import stripe
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def create_checkout_session(request):
-    try:
-        session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
-            line_items=[{
-                'price_data': {
-                    'currency': 'usd',
-                    'product_data': {
-                        'name': 'The Product',
+    if request.method == 'POST':
+        try:
+            session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=[{
+                    'price_data': {
+                        'currency': 'usd',
+                        'product_data': {
+                            'name': 'Animi Dolor Pariatur',
+                        },
+                        'unit_amount': 1000,
                     },
-                    'unit_amount': 9900,
-                },
-                'quantity': 1,
-            }],
-            mode='payment',
-            success_url=request.build_absolute_uri('/thank-you/'),
-            cancel_url=request.build_absolute_uri('/'),
-        )
-        return JsonResponse({'id': session.id})
-    except Exception as e:
-        return JsonResponse({'error': str(e)})
+                    'quantity': 1,
+                },{
+                    'price_data':{
+                        'currency':'usd',
+                        'product_data':{
+                            'name':'Art Deco Home',
+                        },
+                        'unit_amount':3000,
+                    },
+                    'quantity':1,
+                }],
+                mode='payment',
+                success_url=request.build_absolute_uri('/thank-you/?session_id={CHECKOUT_SESSION_ID}'),
+                cancel_url=request.build_absolute_uri('/'),
+            )
+            return JsonResponse({'id': session.id})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
+    return JsonResponse({'error': 'Invalid request method'})
 
 def product_page(request):
-    return render(request, 'product.html', {'stripe_publishable_key': settings.STRIPE_PUBLISHABLE_KEY})
+    return render(request, 'shipping.html', {'stripe_publishable_key': settings.STRIPE_PUBLISHABLE_KEY})
 
 def thank_you_page(request):
     return render(request, 'thank_you.html')
-
